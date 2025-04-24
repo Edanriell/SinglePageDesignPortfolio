299eb8c8fda188777bd97d7ca436c5e91bbdddc5
Edanriell <Edanriell@gmail.com>
2025-04-24 23:45:57 +0300

    feature(app-config): created config

--- Client/nuxt.config.ts
+++ Client/nuxt.config.ts
@@ -1,12 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
+	compatibilityDate: "2024-11-01",
+	devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/image',
-    '@nuxt/test-utils'
-  ]
-})
+	modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/image", "@nuxt/test-utils"],
+
+	css: [
+		"@app/styles/variables.css",
+		"@app/styles/reset.css",
+		"@app/styles/base.css",
+		"@app/styles/styles.css"
+	],
+
+	alias: {
+		"@app": "~/app",
+		"@pages": "~/pages",
+		"@widgets": "~/widgets",
+		"@features": "~/features",
+		"@entities": "~/entities",
+		"@shared": "~/shared"
+	},
+
+	fonts: {
+		families: [
+			{
+				name: "Plus Jakarta Sans",
+				provider: "local",
+				fallbacks: ["Helvetica", "Arial", "sans-serif"],
+				src: [
+					"/fonts/PlusJakartaSans-VariableFont_wght.ttf",
+					"/fonts/PlusJakartaSans-Italic-VariableFont_wght.ttf"
+				]
+			}
+		],
+		processCSSVariables: true
+	}
+});
