e99deb7d8e95257a74da616064b8b530c33be76e
Edanriell <Edanriell@gmail.com>
2025-04-25 16:08:33 +0300

    feature(button): created button component

--- Client/shared/ui/button/ui/button.vue
+++ Client/shared/ui/button/ui/button.vue
@@ -4,11 +4,15 @@
 			type?: "button" | "submit" | "reset";
 			primaryColor?: string;
 			secondaryColor?: string;
+			textPrimaryColor?: string;
+			textSecondaryColor?: string;
 		}>(),
 		{
 			type: "button",
 			primaryColor: "#030303",
-			secondaryColor: "#755cde"
+			secondaryColor: "#755cde",
+			textPrimaryColor: "#fff7f0",
+			textSecondaryColor: "#fff7f0"
 		}
 	);
 </script>
@@ -28,5 +32,25 @@
 
 <style scoped>
 	.button {
+		border-radius: 28rem;
+		margin: 8rem 28rem 8rem 27rem;
+		font-family: var(--font-family);
+		font-weight: 700;
+		font-size: 14rem;
+		line-height: 200%;
+		background: var(--button-primary-color);
+		min-height: 44rem;
+		color: var(--light-cream);
+
+		@media (width >= 768px) {
+			min-height: 56rem;
+			margin: 14rem 45rem 14rem 45rem;
+			font-size: 16rem;
+			line-height: 175%;
+		}
+
+		&:hover {
+			background: var(--button-secondary-color);
+		}
 	}
 </style>
