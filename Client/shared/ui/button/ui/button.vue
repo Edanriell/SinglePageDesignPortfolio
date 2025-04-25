e6c6e7a91045d0af83a52c5be7cb66e519ef2677
Edanriell <Edanriell@gmail.com>
2025-04-25 12:16:57 +0300

    feature(button): working on button component

---
+++ Client/shared/ui/button/ui/button.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+	withDefaults(
+		defineProps<{
+			type?: "button" | "submit" | "reset";
+			primaryColor?: string;
+			secondaryColor?: string;
+		}>(),
+		{
+			type: "button",
+			primaryColor: "#030303",
+			secondaryColor: "#755cde"
+		}
+	);
+</script>
+
+<template>
+	<button
+		:style="{
+			'--button-primary-color': primaryColor,
+			'--button-secondary-color': secondaryColor
+		}"
+		:type="type"
+		class="button"
+	>
+		<slot />
+	</button>
+</template>
+
+<style scoped>
+	.button {
+	}
+</style>
