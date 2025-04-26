4e171e9a201275d72e615dfaf717dbb893868fff
Edanriell <Edanriell@gmail.com>
2025-04-26 10:01:16 +0300

    feature(footer): created footer component

--- Client/widgets/layout/root/ui/root-layout.vue
+++ Client/widgets/layout/root/ui/root-layout.vue
@@ -1,14 +1,12 @@
 <script lang="ts" setup>
 	import { Header } from "@widgets/header/ui";
+	import { Footer } from "@widgets/footer/ui";
 </script>
 
 <template>
 	<Header />
 	<main>
-		<h1>Layout</h1>
 		<slot />
 	</main>
-	<footer>
-		<h2>Footer</h2>
-	</footer>
+	<Footer />
 </template>
