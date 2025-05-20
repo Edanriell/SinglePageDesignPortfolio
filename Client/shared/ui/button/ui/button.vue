6f01c6c5a539af566d782b70328e406372da34b8
Edanriell <Edanriell@gmail.com>
2025-05-20 11:37:08 +0300

    feature(button-circular): refactored button component

--- Client/shared/ui/button/ui/button.vue
+++ Client/shared/ui/button/ui/button.vue
@@ -1,7 +1,8 @@
 <script lang="ts" setup>
-	withDefaults(
+	const props = withDefaults(
 		defineProps<{
 			type?: "button" | "submit" | "reset";
+			shape?: "pill" | "circular";
 			primaryColor?: string;
 			secondaryColor?: string;
 			textPrimaryColor?: string;
@@ -9,16 +10,26 @@
 		}>(),
 		{
 			type: "button",
+			shape: "pill",
 			primaryColor: "#030303",
 			secondaryColor: "#755cde",
 			textPrimaryColor: "#fff7f0",
 			textSecondaryColor: "#fff7f0"
 		}
 	);
+
+	const buttonClasses = computed(() => {
+		return {
+			button: true,
+			"button__shape--pill": props.shape === "pill",
+			"button__shape--circular": props.shape === "circular"
+		};
+	});
 </script>
 
 <template>
 	<button
+		:class="buttonClasses"
 		:style="{
 			'--button-primary-color': primaryColor,
 			'--button-secondary-color': secondaryColor,
@@ -26,7 +37,6 @@
 			'--text-secondary-color': textSecondaryColor
 		}"
 		:type="type"
-		class="button"
 	>
 		<slot />
 	</button>
@@ -34,20 +44,15 @@
 
 <style scoped>
 	.button {
-		border-radius: 28rem;
-		padding: 8rem 28rem 8rem 27rem;
 		font-family: var(--font-family);
 		font-weight: 700;
 		font-size: 14rem;
 		line-height: 200%;
 		background: var(--button-primary-color);
-		min-height: 44rem;
 		color: var(--text-primary-color);
 		text-transform: capitalize;
 
 		@media (width >= 768px) {
-			min-height: 56rem;
-			padding: 14rem 45rem 14rem 45rem;
 			font-size: 16rem;
 			line-height: 175%;
 		}
@@ -56,5 +61,21 @@
 			background: var(--button-secondary-color);
 			color: var(--text-secondary-color);
 		}
+	}
+
+	.button__shape--pill {
+		border-radius: 28rem;
+		padding: 8rem 28rem 8rem 27rem;
+		min-height: 44rem;
+
+		@media (width >= 768px) {
+			min-height: 56rem;
+			padding: 14rem 45rem 14rem 45rem;
+		}
+	}
+
+	.button__shape--circular {
+		border-radius: 50%;
+		padding: 24rem 25rem 24rem 25rem;
 	}
 </style>
